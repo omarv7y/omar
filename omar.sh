9ae2aee1046893a0c1cfcb79f8b7a05e306c5caa
omarv7y <70105391+omarv7y@users.noreply.github.com>
2020-08-23 19:44:38 +0300

    Add files via upload
    مرحبا شباب معكم الاداه اداه عمر القرصان
    طلب مساعده من هنا   https://chat.whatsapp.com/GVUPFI3cNBVKmmtJVG6ea3

---
+++ omar.sh
@@ -0,0 +1,112 @@
+####################################################################
+#####>>>   https://chat.whatsapp.com/GVUPFI3cNBVKmmtJVG6ea31                                                              #
+#####      ☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️☝️                                                        #
+#####      💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟💟       #
+#####                                                              #
+#####                                                              #
+#####                                                              #
+######################################################################
+
+red='\e[1;31m'                                                                                       green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'                                                                                    cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'                                                                                    clear
+echo -e $green "-------------------------------------------------------"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-          $>>>OMAR>😘😘😘🥳🥳😱😻😻😻👇👇👇 $green   -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-          $>>>OMAR>>😍😍😍😍😍😍💟🥳🤠 👍👍👍$green  -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-------------------------------------------------------"
+sleep 0.5
+echo ""
+echo -e $red     "OMAR<<<|>>>OMAR"
+echo
+echo -e $green   "1- Linux & Gnuroot"
+echo
+echo -e $green   "2- Termux"
+echo
+echo -e $green   "0- Exit"
+read use
+if [ $use = 2 ]
+then
+clear
+pkg update -y
+pkg upgrade -y
+termux-setup-storage -y
+pkg install python -y
+pkg install python2 -y
+pkg install python2-dev -y
+pkg install python3 -y
+pkg install java -y
+pkg install fish -y
+pkg install ruby -y
+pkg install help -y
+pkg install git -y
+pkg install host -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install havij -y
+pkg install hydra -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install google -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wireshark -y
+pkg install wgetrc -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install vpn -y
+pkg install unrar -y
+pkg install toilet -y
+pkg install proot -y
+pkg install net-tools -y
+pkg install golang -y
+pkg install chroot -y
+termux-chroot -y
+pkg install macchanger -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+termux-setup-storage 
+pkg install macchanger -y
+apt update && apt upgrade -y
+fi
+if [ $use = 1 ]
+then
+echo "😢😢😢😢😢😥😥😥😥"
+echo "قريبا<<<<<<> 😥😥"
+echo " l>>>help>>  👉👉👉👉https://chat.whatsapp.com/GVUPFI3cNBVKmmtJVG6ea3 "
+exit
+exit
+bash install.sh
+fi
+if [ $use = 0 ]
+then
+clear
+exit
+else
+bash install.sh 
+fi 
+
